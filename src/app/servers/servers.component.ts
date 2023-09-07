f783a82b1c37efc523848b63883aaab3a2c9f74c
enes.ozdemir <enes.ozdemir@epati.com.tr>
2023-09-07 17:43:00 +0300

    componentler ve eventler ilgili alıştırmalar

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -2,9 +2,26 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
+  // selector: '[app-servers]'
+  // selector: '.app-servers'
   templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent {
+  allowNewServer = false;
+  serverCreationStatus = 'Oluşturulan servis yok!';
+  serverName = '';
+  serverCreated = false;
+
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+  }
+
+  onCreateServer() {
+    this.serverCreated = true;
+    this.serverCreationStatus = 'server oluşturuldu, server adı: ' + this.serverName;
+  }
 
 }
